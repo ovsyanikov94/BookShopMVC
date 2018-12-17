e79711e2f8c1d6d4ea482b226268c6e486590a5d
dreamcast <slipdoll@gmail.com>
2018-12-17 21:14:59 +0300

    backend login started

--- Application/Services/AuthorizeService.php
+++ Application/Services/AuthorizeService.php
@@ -10,7 +10,7 @@ class AuthorizeService{
 
     public function LogIn( $login, $password ){
 
-        $stm = MySQL::$db->prepare("SELECT * FROM users WHERE userLogin = :login or userEmail = :login, userPassword = :password");
+        $stm = MySQL::$db->prepare("SELECT * FROM users WHERE userLogin = :login or userEmail = :login and userPassword = :password");
         $stm->bindParam(':login', $login, \PDO::PARAM_STR);
         $stm->bindParam(':password', $password, \PDO::PARAM_STR);
         $stm->execute();
