cda4771cb555247a591b5f1f7a9b0dbcab252b9e
UlitkaCheshir <OLGA151008OLGA>
2018-12-19 23:15:07 +0300

    1

--- public/js/authors.js
+++ public/js/authors.js
@@ -6,18 +6,31 @@
 
         let nameAuthor = $('#nameAuthor').val();
         let lastNameAuthor = $('#lastNameAuthor').val();
+        console.log('nameAuthor:' , nameAuthor);
+        console.log('lastNameAuthor:' , lastNameAuthor);
+        console.log('lastNameAuthor:' , lastNameAuthor);
 
-        $.post(
-            `${window.paths.AjaxServerUrl}${window.paths.AddAuthor}`,
-            {
-                'authorLastname': lastNameAuthor,
-                'authorFirstname': nameAuthor
-            },
-            function ( response ){
+        if(!/^[a-zа-я]{4,50}$/i.test(nameAuthor) || !/^[a-zа-я]{4,50}$/i.test(lastNameAuthor)){
+            $('#successMessage').fadeOut(1000);
+            $('#errorMessage').fadeOut(500);
+            $('#errorInput').fadeIn(500);
+        }//if
+        else{
+            $.post(
+                `${window.paths.AjaxServerUrl}${window.paths.AddAuthor}`,
+                {
+                    'authorLastname': lastNameAuthor,
+                    'authorFirstname': nameAuthor
+                },
+                function ( response ){
 
-                console.log('response:' , response);
+                    $('#successMessage').fadeIn(1000);
+                    $('#errorMessage').fadeOut(500);
+                    $('#errorInput').fadeOut(500);
 
-                $('#authorTable').append(`
+                    console.log('response:' , response);
+
+                    $('#authorTable').append(`
                     <tr data-author-id = "${response.authorID}">
                         <td>${response.authorID}</td>
                         <td>${nameAuthor}</td>
@@ -29,10 +42,13 @@
                             <a href="${window.paths.AjaxServerUrl}author/${response.authorID}" class="btn btn-primary" >Обновить</a>
                         </td>
                     </tr>`
-                );
+                    );
 
-            }//fn
-        );
+                }//fn
+            );
+
+        }//else
+
 
 
     }  );
@@ -40,30 +56,47 @@
     $('body').on('click','#removeAuthor,.btn-danger' , function (  ){
 
         let authorID = +$( this ).data('author-id');
+        let authorName = $( this ).data('author-name');
+        let authorLastName = $( this ).data('author-lastname');
 
         let deleteURL = `${window.paths.AjaxServerUrl}${window.paths.RemoveAuthor}`;
         deleteURL = deleteURL.replace(':authorID' , authorID);
 
         let self = $(this);
 
-        $.ajax({
-            'url': deleteURL,
-            'type': 'DELETE',
-            'success': ( data )=>{
-
-                if( +data.code === 200 ){
-
-                    if( self.attr('id') === 'removeAuthor' ){
-                        location.href = `${window.paths.AjaxServerUrl}authors`;
-                    }//if
-                    else{
-                        $(`tr[data-author-id=${authorID}]`).remove();
-                    }//else
-
-                }//if
-
-            }//success
+        $('#login-form').modal({
+            showClose : false
         });
+
+        $('#nameAuthorModal').text(authorName);
+        $('#lastNameAuthorModal').text(authorLastName);
+        
+        
+        $('#closeModal').click(function () {
+            $.modal.close();
+        });
+
+        $('#deleteAuthorModal').click(function () {
+            $.ajax({
+                'url': deleteURL,
+                'type': 'DELETE',
+                'success': ( data )=>{
+
+                    if( +data.code === 200 ){
+
+                        if( self.attr('id') === 'removeAuthor' ){
+                            location.href = `${window.paths.AjaxServerUrl}authors`;
+                        }//if
+                        else{
+                            $(`tr[data-author-id=${authorID}]`).remove();
+                        }//else
+
+                    }//if
+                    $.modal.close();
+                }//success
+            });
+        });
+
 
     }  );
 
