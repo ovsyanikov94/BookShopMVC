0e133fda9591111c45a2bef0c66aa862e7d53cfe
dreamcast <slipdoll@gmail.com>
2019-01-09 23:40:42 +0300

    personal profile page complete

--- Application/Controllers/PersonalPageController.php
+++ Application/Controllers/PersonalPageController.php
@@ -116,33 +116,6 @@ class PersonalPageController extends BaseController {
     //страница изменения личной информации пользователя
     public function EditPersonalDataAction(){
 
-//        $template = $this->twig->load('PersonalPage/edit-personal-data.twig');
-//
-//        try{
-//
-//            if( isset($_COOKIE["cookie_user"]) ){
-//
-//                $CookieUser = unserialize($_COOKIE["cookie_user"]);
-//                echo $template->render( array( 'user' => $CookieUser ) );
-//
-//            }//if
-//            else{
-//
-//                echo $template->render( array( 'user' => $_SESSION["session_user"] ) );
-//
-//            }//else
-//
-//        }//try
-//        catch(\Exception $ex){
-//
-//            echo "<pre>";
-//            print_r($ex);
-//            echo "<pre>";
-//
-//            include '../Application/Views/Errors/InternalError.php';
-//
-//        }//catch
-
         $template = $this->twig->load('PersonalPage/edit-personal-data.twig');
 
         try{
